809f716aa38640392994b6dec45f83ed6927ea6c
Zineb-S <75624026+Zineb-S@users.noreply.github.com>
2025-01-07 12:02:43 +0100

    metrics

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const itemRoutes = require('./routes/itemRoutes');
 const cors = require('cors');
+const promClient = require('prom-client');
 
 const app = express();
 const port = 5000;
@@ -12,7 +13,14 @@ app.use(cors());
 
 // Routes
 app.use('/api', itemRoutes);
+const register = new promClient.Registry();
+promClient.collectDefaultMetrics({ register });
 
+// Expose metrics endpoint for Prometheus
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 // Start Server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
